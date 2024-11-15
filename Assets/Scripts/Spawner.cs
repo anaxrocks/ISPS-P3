97c165df6ae35b3fef96fe672f324a74f51fcec7
tntieu <tntieu@stanford.edu>
2024-11-15 15:59:49 -0800

    Added spawner

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public GameObject[] packages;
+    public float timetoSpawn, spawnCountdown;
+
+    void Start() 
+    {
+        spawnCountdown = timetoSpawn;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        spawnCountdown -= Time.deltaTime;
+        if (spawnCountdown <= 0)
+        {
+            spawnCountdown = timetoSpawn;
+            int randomPackage = Random.Range(0, packages.Length);
+            Vector3 randomSpawnPosition = new Vector3(Random.Range(-9,9), 8, 0);
+
+            Instantiate(packages[randomPackage], randomSpawnPosition, Quaternion.identity);
+        }
+    }
+}
