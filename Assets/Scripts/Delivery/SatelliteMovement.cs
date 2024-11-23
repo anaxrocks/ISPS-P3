7c09fe75f66508e879fae144b8fdff5277e61e57
tntieu <tntieu@stanford.edu>
2024-11-22 17:25:33 -0800

    receiver moves up and down now too huehue

--- Assets/Scripts/Delivery/SatelliteMovement.cs
+++ Assets/Scripts/Delivery/SatelliteMovement.cs
@@ -8,15 +8,23 @@ public class SatelliteMovement : MonoBehaviour
 
     public float moveSpeed;
     public float deadZone = -45;
+    private float time;
+    private Vector3 spawnPos;
     void Start()
     {
-        
+        time = 0;
+        spawnPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        time += Time.deltaTime;
+        float x = 3 * time ; // x moves linearly with time
+        float y = 0.2f * Mathf.Sin(5 * time); // y moves sinusoidally with time
+
+        transform.position = spawnPos - new Vector3(x, y);
+        // transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
 
         if (transform.position.x < deadZone) {
             Destroy(gameObject);
