4dfb94ec31d84cd078963e6337a039ad7d5d74ea
Ana Nguyen <anax@stanford.edu>
2024-12-06 01:04:29 -0800

    Hover description implemented for first upgrade (waiting on icons)

---
+++ Assets/Scripts/Upgrades/HoverDescription.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class HoverDescription : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [TextArea]
+    public string description; // The description for this upgrade
+    public TextMeshProUGUI descriptionText; // Reference to the TextMeshPro UI component
+    public RectTransform tooltipPanel; // Reference to the Panel (background)
+
+    void Update()
+    {
+        if (descriptionText.gameObject.activeSelf)
+        { 
+            Vector3 mousePosition = Input.mousePosition;
+            descriptionText.transform.position = mousePosition + new Vector3(100, 0, 0); // Offset for visibility
+            tooltipPanel.transform.position = mousePosition + new Vector3(100, 0, 0);
+        }
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        // Show the description when the pointer enters the object
+        if (descriptionText != null)
+        {
+            descriptionText.text = description;
+            descriptionText.gameObject.SetActive(true);
+            tooltipPanel.gameObject.SetActive(true);
+        }
+        
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        // Hide the description when the pointer exits the object
+        if (descriptionText != null)
+        {
+            descriptionText.text = "";
+            descriptionText.gameObject.SetActive(false);
+            tooltipPanel.gameObject.SetActive(false);
+        }
+    }
+}
